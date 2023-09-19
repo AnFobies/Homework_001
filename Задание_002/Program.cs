678178e1bbec0c20ae66bd2e95bf7b5154ffae43
Anton <toshasuhanov@ya.ru>
2023-09-19 08:27:32 +0300

    Добавили Задание_002

---
+++ Задание_002/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Программа, которая на вход принимает два числа и выдаёт, 
+какое число большее, а какое меньшее*/
+
+Console.WriteLine("Укажите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Укажите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if(number1 > number2)
+{
+    Console.WriteLine($"Наибольшее число {number1}, а наименьшее {number2}");
+}
+else
+{
+    Console.WriteLine($"Наибольшее число {number2}, а наименьшее {number1}");
+}
