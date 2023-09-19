8cf7865dc5a29249a5da5294fffda4f1f47183fe
Anton <toshasuhanov@ya.ru>
2023-09-19 09:06:18 +0300

    Добавили Задание_006

---
+++ Задание_006/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Программа, которая на вход принимает число и выдаёт, 
+является ли оно чётным*/
+
+Console.WriteLine("Укажите число: ");
+int number1 = int.Parse(Console.ReadLine());
+
+if(number1%2 == 0)
+{
+    Console.WriteLine("Это число является четным!");
+}
+else
+{
+    Console.WriteLine("Это число является нечетным!");
+}
