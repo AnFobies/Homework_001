c1787a96b03677d5be39afcc9b26bf017766eb46
Anton <toshasuhanov@ya.ru>
2023-09-19 09:18:14 +0300

    Добавили Задание_008

---
+++ Задание_008/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Программа, которая на вход принимает число (N) 
+и на выходе показывает все чётные числа от 1 до N*/
+
+Console.WriteLine("Укажите число: ");
+int number = int.Parse(Console.ReadLine());
+int count = 2;
+
+if(number < 2)
+{
+    Console.Write($"Четных чисел от одного до 1 до {number}");
+}
+
+if(number >= 2)
+{
+    do
+    {
+        Console.Write($"{count}, ");
+        count = count + 2;
+    }
+    while(count <= number);
+}
